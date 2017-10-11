83d23d2c1fb974599bbdcc5ac1df0d7bac89fc1f
gunthernic <gunthernic@eisti.eu>
2017-10-12 00:55:41 +0200

    Add sample template website

---
+++ src/main/java/fr/gunther/glorybox/website/controller/WebController.java
@@ -0,0 +1,16 @@
+package fr.gunther.glorybox.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+        @GetMapping("/")
+        public ModelAndView displayMainPage() {
+                return new ModelAndView("index");
+        }
+}
