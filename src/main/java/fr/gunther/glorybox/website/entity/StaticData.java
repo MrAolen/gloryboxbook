b616ddf2518ea2bc8e2b00a88d7953a275d559f3
gunthernic <gunthernic@eisti.eu>
2017-10-24 23:34:49 +0200

    Edit staticdata entity

--- src/main/java/fr/gunther/glorybox/website/entity/StaticData.java
+++ src/main/java/fr/gunther/glorybox/website/entity/StaticData.java
@@ -2,17 +2,23 @@ package fr.gunther.glorybox.website.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
-@Entity(name="staticdata")
+@Entity(name="static_datas_website")
 public class StaticData {
-        @Column(name="label")
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name="id")
+        private Long id;
+
+        @Column(name="value_staticdata")
         private String label;
 
-        @Column(name="key")
-        @Id
+        @Column(name="key_staticdata")
         private String key;
 
         public String getLabel() {
@@ -29,5 +35,13 @@ public class StaticData {
 
         public void setKey(String key) {
                 this.key = key;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
         }
 }
