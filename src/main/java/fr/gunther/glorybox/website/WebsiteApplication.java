55a6b9035a58c404bf6c7bbd5921c12eb585bcaf
gunthernic <gunthernic@eisti.eu>
2017-10-24 22:41:08 +0200

    Add database prod

--- src/main/java/fr/gunther/glorybox/website/WebsiteApplication.java
+++ src/main/java/fr/gunther/glorybox/website/WebsiteApplication.java
@@ -1,12 +1,14 @@
 package fr.gunther.glorybox.website;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsiteApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		SpringApplication.run(WebsiteApplication.class, args);
 	}
 }
